4677ea797e7ba08bb487560db76db8ba4c37497e
my account <taberihun07@gmail.com>
2023-05-28 13:10:52 -0700

    render works

--- src/main/java/com/example/demo/PdfReaderController.java
+++ src/main/java/com/example/demo/PdfReaderController.java
@@ -2,11 +2,13 @@ package com.example.demo;
 
 import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.ScrollPane;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
 import org.apache.pdfbox.pdmodel.PDDocument;
@@ -16,13 +18,38 @@ import org.apache.pdfbox.rendering.PDFRenderer;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class PdfReaderController {
+public class PdfReaderController implements Initializable {
     @FXML
     private ScrollPane scrollPane;
 
     @FXML
+    private AnchorPane anchorPane;
+
+    @FXML
+    private ToolBar toolBar;
+
+    @FXML
+    private ListView listView;
+
+
+    @FXML
     private AnchorPane pdfPagesAnchorPane;
+
+    @FXML
+    private AnchorPane headerAnchorPane;
+
+    @FXML
+    private TextField pageNumberTextField;
+
+    @FXML
+    private MenuBar menuBar;
+
+    @FXML
+    private VBox rightSidebar;
+
 
     @FXML
     private void handleOpenAction() {
@@ -58,7 +85,7 @@ public class PdfReaderController {
 
     private void renderPdf(File file) throws IOException {
         PDDocument document = PDDocument.load(file);
-        PDFRenderer pdfRenderer =new PDFRenderer(document);
+        PDFRenderer pdfRenderer = new PDFRenderer(document);
 
         double yPosition = 0.0;
 
@@ -71,13 +98,6 @@ public class PdfReaderController {
             imageView.setLayoutY(yPosition);
             pdfPagesAnchorPane.getChildren().add(imageView);
             yPosition += imageView.getBoundsInLocal().getHeight();
-
-            // Free memory by removing image data from the BufferedImage
-            image.flush();
-            image = null;
-
-            // Free memory by removing image data from the PDFRenderer
-            pdfRenderer.clearResources();
         }
 
         document.close();
@@ -88,5 +108,29 @@ public class PdfReaderController {
         final int dpi = 150;
         BufferedImage bufferedImage = pdfRenderer.renderImageWithDPI(pageIndex, dpi);
         return javafx.embed.swing.SwingFXUtils.toFXImage(bufferedImage,null);
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // Set the anchor constraints for the root node
+        AnchorPane.setTopAnchor(anchorPane, 0.0);
+        AnchorPane.setBottomAnchor(anchorPane, 0.0);
+        AnchorPane.setLeftAnchor(anchorPane, 0.0);
+        AnchorPane.setRightAnchor(anchorPane, 0.0);
+
+        // Set the HBox constraints for the scrollPane content
+        HBox.setHgrow(pdfPagesAnchorPane, Priority.ALWAYS);
+
+        // Set the VBox constraints for the listView
+        VBox.setVgrow(listView, Priority.ALWAYS);
+
+        // Set the VBox constraints for the rightSidebar
+        VBox.setVgrow(rightSidebar, Priority.ALWAYS);
+
+        // Set the headerAnchorPane to stretch horizontally
+        headerAnchorPane.setMaxWidth(Double.MAX_VALUE);
+
+        // Set the pageNumberTextField to stretch horizontally
+        pageNumberTextField.setMaxWidth(Double.MAX_VALUE);
     }
 }
