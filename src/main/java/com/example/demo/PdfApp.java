11adff7a99a23fd634dd6c2b0fc20d6ddefe22fa
fekadusisay21 <fekadusisay07@gmail.com>
2023-06-05 12:46:54 +0300

    about us page added

--- src/main/java/com/example/demo/PdfApp.java
+++ src/main/java/com/example/demo/PdfApp.java
@@ -2,19 +2,24 @@ package com.example.demo;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
+import javax.swing.tree.DefaultTreeModel;
 import java.io.IOException;
+import java.util.Objects;
 
 
 public class PdfApp extends Application {
 
+    private static Scene scene;
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(PdfApp.class.getResource("pdfReader.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 900, 800);
+         scene = new Scene(fxmlLoader.load(), 900, 800);
         PdfReaderController controller = fxmlLoader.getController();
         controller.initialize(null, null);
         stage.setTitle("Pdf Reader");
@@ -27,8 +32,15 @@ public class PdfApp extends Application {
     }
 
     public static void main(String[] args) {
-
         launch();
+    }
+
+    public static void sceneFactory(String fxml) throws IOException{
+        scene.setRoot(loadFXML(fxml));
+        scene.getWindow().sizeToScene();
+    }
+    private static Parent loadFXML(String fxml) throws IOException {
+        return FXMLLoader.load(Objects.requireNonNull(PdfApp.class.getResource(fxml + ".fxml")));
     }
 
 }
